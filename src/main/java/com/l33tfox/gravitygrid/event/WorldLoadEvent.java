a8eb49445104acefdabaf06f6f3c6a6130f95ac8
L33tfox <leetfox8@gmail.com>
2025-05-24 20:24:29 -0400

    restructured fair spawnpoint method

--- src/main/java/com/l33tfox/gravitygrid/event/WorldLoadEvent.java
+++ src/main/java/com/l33tfox/gravitygrid/event/WorldLoadEvent.java
@@ -10,12 +10,9 @@ public class WorldLoadEvent {
     public static int chunkLength = 16;
 
     public static void setFairSpawnpoint(MinecraftServer server, ServerWorld world) {
-        int spawnChunkX = world.getSpawnPos().getX() / chunkLength;
-        int spawnChunkZ = world.getSpawnPos().getZ() / chunkLength;
-
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                WorldChunk nearbySpawnChunk = world.getChunk(spawnChunkX + i, spawnChunkZ + j);
+                WorldChunk nearbySpawnChunk = world.getWorldChunk(world.getSpawnPos().add(chunkLength * i, 0, chunkLength * j));
 
                 if (nearbySpawnChunk.getAttachedOrCreate(ModAttachmentTypes.GRAVITY_DIRECTION_TYPE) != Direction.DOWN.getId()) {
                     nearbySpawnChunk.setAttached(ModAttachmentTypes.GRAVITY_DIRECTION_TYPE, Direction.DOWN.getId());
