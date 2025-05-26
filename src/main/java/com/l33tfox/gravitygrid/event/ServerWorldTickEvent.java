1892fdf0c133c0ac2fff9cb1f3b7dc922abca409
L33tfox <leetfox8@gmail.com>
2025-05-25 21:25:52 -0400

    fixed bug where gravity not resetting to down when gamerule changed to disabled

--- src/main/java/com/l33tfox/gravitygrid/event/ServerWorldTickEvent.java
+++ src/main/java/com/l33tfox/gravitygrid/event/ServerWorldTickEvent.java
@@ -9,11 +9,30 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.chunk.WorldChunk;
 
 public class ServerWorldTickEvent {
+    public static boolean gravityWasReset = false;
+
     public static void playersWorldTick(ServerWorld world) {
         if (!world.getGameRules().getBoolean(ModGameRules.GRAVITY_GRID_ENABLED)) {
+            if (!gravityWasReset) {
+                resetGravityForAllPlayers(world);
+                gravityWasReset = true;
+            }
             return;
         }
 
+        gravityWasReset = false;
+        setGravityDirectionForAllPlayers(world);
+    }
+
+    private static void resetGravityForAllPlayers(ServerWorld world) {
+        for (ServerPlayerEntity player : world.getPlayers()) {
+            if (!player.isSpectator()) {
+                GravityChangerAPI.setBaseGravityDirection(player, Direction.DOWN);
+            }
+        }
+    }
+
+    private static void setGravityDirectionForAllPlayers(ServerWorld world) {
         for (ServerPlayerEntity player : world.getPlayers()) {
             if (!player.isSpectator()) {
                 WorldChunk chunk = world.getChunk(player.getChunkPos().x, player.getChunkPos().z);
